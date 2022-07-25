1049a3fcb2957624647a00af9be7d7d5f06bf8fc
Alexandr Osipov <ossipov44rus@gmail.com>
2022-07-25 16:46:39 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿ChangeXYZ(10, 20, 30);
+
+void ChangeXYZ(int x, int y, int z)
+{
+    x = x * y * z;
+    z = x / y / z;
+    y = x / y / z;
+    x = x / y / z;
+    Console.WriteLine($"X:{x},Y:{y},Z:{z}");
+}
